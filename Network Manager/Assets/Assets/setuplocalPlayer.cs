9b193ea8459d8b63e231d771f221eb0564621347
Faraz Khalid <farazkhalid05@yahoo.com>
2020-10-10 23:37:55 +0500

    created local controls for each client

---
+++ Network Manager/Assets/Assets/setuplocalPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class setuplocalPlayer : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isLocalPlayer)
+        {
+            GetComponent<PlayerController>().enabled = true;
+        }
+        else
+        {
+            GetComponent<PlayerController>().enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
